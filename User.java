b7283804ad9afea96ddf3c064435aa011c15443b
dipesh2901 <67797922+dipesh2901@users.noreply.github.com>
2021-08-31 12:02:59 +0530

    Program44: CRUD Operations Using Spring Data JPA and MySQL

--- User.java
+++ User.java
@@ -1,43 +1,20 @@
 package com.neosoft.springboot.bean;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int uid;
 	private String uname;
 	private String upass;
-
-	public User() {
-
-	}
-
-	public User(int uid, String uname, String upass) {
-		this.uid = uid;
-		this.uname = uname;
-		this.upass = upass;
-	}
-
-	public int getUid() {
-		return uid;
-	}
-
-	public void setUid(int uid) {
-		this.uid = uid;
-	}
-
-	public String getUname() {
-		return uname;
-	}
-
-	public void setUname(String uname) {
-		this.uname = uname;
-	}
-
-	public String getUpass() {
-		return upass;
-	}
-
-	public void setUpass(String upass) {
-		this.upass = upass;
-	}
 
 }
